69d6c032c01b971d0222798573865a9e81d9b738
Owen Ashton <oashton@live.co.uk>
2026-02-12 16:37:24 +0000

    update nav bar slightly and start on content

---
+++ src/lib/navbarProperties.ts
@@ -0,0 +1,8 @@
+export const navbarProperties = [
+	{ "aria-label": "Welcome / Homepage", text: "Welcome / Homepage", href: "/" },
+	{ "aria-label": "Venue / Directions / Places to Stay", text: "Venue / Directions / Places to Stay", href: "/the-day" },
+	{ "aria-label": "Accommodation Section", text: "Accommodation", href: "/accommodation" },
+	{ "aria-label": "Transport Section", text: "Transport", href: "/transport" },
+	{ "aria-label": "Gifts Section", text: "Gifts", href: "/gifts" },
+	{ "aria-label": "The Send Off Section", text: "The Send Off", href: "/send-off" },
+];
