e63d622487410bd130703c2d9d5b68b4f549f1c7
Owen Ashton <oashton@live.co.uk>
2026-02-12 17:30:19 +0000

    Finish adding the new content

--- src/components/Schedule/Schedule.tsx
+++ src/components/Schedule/Schedule.tsx
@@ -1,13 +1,12 @@
 import {FC, useState, useEffect} from 'react';
 import ScheduleItem from './ScheduleItem';
 import './Schedule.css';
-const transportPickUp = '/icons/scheduleIcons/007-bus.png';
-const churchIcon = '/icons/scheduleIcons/002-church.png'
-const ceremonyIcon = '/icons/scheduleIcons/004-diamond.png'
-const cocktailsIcon = '/icons/scheduleIcons/001-martini.png'
-const weddingBreakfastIcon = '/icons/scheduleIcons/005-wedding-dinner.png'
-const partyIcon = '/icons/scheduleIcons/006-disco-ball.png'
-const carriagesIcon = '/icons/scheduleIcons/003-wedding-car.png'
+const martiniIcon = '/icons/scheduleIcons/001-martini.png';
+const ceremonyIcon = '/icons/scheduleIcons/004-diamond.png';
+const photoIcon = '/icons/generalSections/002-camera.png';
+const dinnerIcon = '/icons/scheduleIcons/005-wedding-dinner.png';
+const discoIcon = '/icons/scheduleIcons/006-disco-ball.png';
+const carriagesIcon = '/icons/scheduleIcons/003-wedding-car.png';
 
 const Schedule = () : FC<{}> =>{
     const [isMobile, setMobileView] = useState(true);
@@ -32,46 +31,58 @@ const Schedule = () : FC<{}> =>{
     return (
         <div className={isMobile?"schedule-container-mobile":"schedule-container"}>
             <ScheduleItem
-                iconUrl={transportPickUp}
-					text={"Book your own taxis"}
-                    time={"1:00pm"}
-					index={1}
-					isLast={false}/>
-            <ScheduleItem
-                iconUrl={churchIcon}
-					text={"Guest Arrival"}
-                    time={"1:30pm"}
-					index={2}
-					isLast={false}/>
+                iconUrl={martiniIcon}
+                text={"Arrivals & Welcome Drinks"}
+                time={"12:30pm"}
+                index={1}
+                isLast={false}/>
             <ScheduleItem
                 iconUrl={ceremonyIcon}
-					text={"Ceremony"}
-                    time={"2:00pm"}
-					index={3}
-					isLast={false}/>
+                text={"Ceremony"}
+                time={"1:30pm"}
+                index={2}
+                isLast={false}/>
             <ScheduleItem
-                iconUrl={cocktailsIcon}
-					text={"Cocktail Reception"}
-                    time={"3:30pm"}
-					index={4}
-					isLast={false}/>
+                iconUrl={photoIcon}
+                text={"Photos & Garden/Pub Games"}
+                time={"3:00pm"}
+                index={3}
+                isLast={false}/>
             <ScheduleItem
-                iconUrl={weddingBreakfastIcon}
-					text={"Wedding Breakfast"}
-                    time={"5:30pm"}
-					index={5}
-					isLast={false}/>
+                iconUrl={dinnerIcon}
+                text={"Dinner & Drinks"}
+                time={"4:00pm"}
+                index={4}
+                isLast={false}/>
             <ScheduleItem
-                iconUrl={partyIcon}
-					text={"Party Time!"}
-                    time={"8:00pm"}
-					index={6}
-					isLast={false}/>
+                iconUrl={martiniIcon}
+                text={"Evening Guests Arrive"}
+                time={"6:30pm"}
+                index={5}
+                isLast={false}/>
+            <ScheduleItem
+                iconUrl={discoIcon}
+                text={"First Dance & Disco"}
+                time={"7:00pm"}
+                index={6}
+                isLast={false}/>
+            <ScheduleItem
+                iconUrl={dinnerIcon}
+                text={"Pizzas"}
+                time={"8:45pm"}
+                index={7}
+                isLast={false}/>
+            <ScheduleItem
+                iconUrl={martiniIcon}
+                text={"Last Orders"}
+                time={"11:15pm"}
+                index={8}
+                isLast={false}/>
             <ScheduleItem
                 iconUrl={carriagesIcon}
                 text={"Carriages"}
                 time={"12:00am"}
-                index={7}
+                index={9}
                 isLast={true}/>
         </div>
         )
--- src/lib/navbarProperties.ts
+++ src/lib/navbarProperties.ts
@@ -1,8 +1,6 @@
 export const navbarProperties = [
 	{ "aria-label": "Welcome / Homepage", text: "Welcome / Homepage", href: "/" },
 	{ "aria-label": "Venue / Directions / Places to Stay", text: "Venue / Directions / Places to Stay", href: "/the-day" },
-	{ "aria-label": "Accommodation Section", text: "Accommodation", href: "/accommodation" },
-	{ "aria-label": "Transport Section", text: "Transport", href: "/transport" },
-	{ "aria-label": "Gifts Section", text: "Gifts", href: "/gifts" },
-	{ "aria-label": "The Send Off Section", text: "The Send Off", href: "/send-off" },
+	{ "aria-label": "Schedule", text: "Schedule", href: "/schedule" },
+	{ "aria-label": "FAQs", text: "FAQs", href: "/faqs", subDetails: ["GIFTS", "DRESS CODE", "EVE GUESTS", "KIDS", "PLUS ONES"] },
 ];
